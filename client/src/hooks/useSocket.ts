f6bfcb02968e800674938fc9f8309724cd0e16ff
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-01-30 01:16:40 +0530

    Set up Socket.io Client (Basic connection hook)

---
+++ client/src/hooks/useSocket.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { socket } from '../socket';
+
+export const useSocket = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
+
+  return { socket, isConnected };
+};
